a2461f49cba2525878d6d2a70f52a14a1e5fc447
wxyBUPT <746451950@qq.com>
2016-08-31 19:46:07 +0800

    231

---
+++ src/bitManipulation/Solution_231.java
@@ -0,0 +1,17 @@
+package bitManipulation;
+
+/**
+ * Created by xiyuanbupt on 8/31/16.
+
+ Given an integer, write a function to determine if it is a power of two.
+ */
+public class Solution_231 {
+    public boolean isPowerOfTwo(int n){
+        if(n<=0)return false;
+        while(n!=1){
+            if(n%2==1)return false;
+            n /=2;
+        }
+        return true;
+    }
+}
