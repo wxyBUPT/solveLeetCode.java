06006eef78aea47739a5b441882e37375fd05031
wxyBUPT <746451950@qq.com>
2016-09-13 15:51:15 +0800

    判断链表是不是会问，时间和空间复杂度都是 n，据说还有时间复杂度为 n 空间复杂度为 1 的算法

---
+++ src/list/Solution_234.java
@@ -0,0 +1,33 @@
+package list;
+
+import java.util.List;
+
+/**
+ * Created by xiyuanbupt on 9/13/16.
+ Given a singly linked list, determine if it is a palindrome.
+
+ Follow up:
+ Could you do it in O(n) time and O(1) space?
+ */
+public class Solution_234 {
+    public boolean isPalindrome(ListNode head) {
+        ListNode tmp = head;
+        int len = 0;
+        while (tmp!=null){
+            tmp = tmp.next;
+            len ++;
+        }
+        int[] re = new int[len];
+        tmp = head;
+        int i = 0;
+        while (tmp!=null){
+            re[i++] = tmp.val;
+            tmp = tmp.next;
+        }
+        int mid = len/2;
+        for(i = 0;i<mid;i++){
+            if(re[i]!=re[len-i-1])return false;
+        }
+        return  true;
+    }
+}
