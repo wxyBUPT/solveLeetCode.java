132713da374156d271eb2978ad318a51a86988a1
wxyBUPT <746451950@qq.com>
2016-09-13 16:22:59 +0800

    使用hashmap 判断string 字母与字母的个数是否相同

---
+++ src/string/Solution_242.java
@@ -0,0 +1,42 @@
+package string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by xiyuanbupt on 9/13/16.
+ Given two strings s and t, write a function to determine if t is an anagram of s.
+
+ For example,
+ s = "anagram", t = "nagaram", return true.
+ s = "rat", t = "car", return false.
+ */
+
+public class Solution_242 {
+
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())return false;
+        Map<Character,Integer> c_c = new HashMap<>();
+        int len = s.length();
+        for(int i = 0;i<len;i++){
+            Character c = s.charAt(i);
+            Integer count = c_c.get(c);
+            if(count==null){
+                count = 1;
+            }else {
+                count += 1;
+            }
+            c_c.put(c,count);
+        }
+
+        for(int i = 0;i<len;i++){
+            Character c = t.charAt(i);
+            Integer count = c_c.get(c);
+            if(count == null)return false;
+            count --;
+            if(count<0)return false;
+            c_c.put(c,count);
+        }
+        return true;
+    }
+}
