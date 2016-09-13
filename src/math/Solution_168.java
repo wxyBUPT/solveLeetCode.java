f01e516051c4df51be48865862eeb29dd1a85c00
wxyBUPT <746451950@qq.com>
2016-09-13 21:22:46 +0800

    excel 表格上方标题，

---
+++ src/math/Solution_168.java
@@ -0,0 +1,31 @@
+package math;
+
+/**
+ * Created by xiyuanbupt on 9/13/16.
+ Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+ For example:
+
+ 1 -> A
+ 2 -> B
+ 3 -> C
+ ...
+ 26 -> Z
+ 27 -> AA
+ 28 -> AB
+ */
+public class Solution_168 {
+    public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+        while (n!=0){
+            sb.insert(0,(char)('A' + (n-1)%26 ));
+            n = (n-1) /26;
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args){
+        Solution_168 sl = new Solution_168();
+        System.out.println(sl.convertToTitle(28));
+    }
+}
