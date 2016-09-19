f220929b6643c65c25792532d4c45de7fc12b3f6
wxyBUPT <746451950@qq.com>
2016-09-19 11:16:08 +0800

    链表重新排序操作

---
+++ src/list/Solution_143.java
@@ -0,0 +1,49 @@
+package list;
+
+/**
+ * Created by xiyuanbupt on 9/18/16.
+ */
+public class Solution_143 {
+    public void reorderList(ListNode head) {
+        if(head==null||head.next==null) return;
+
+        //Find the middle of the list
+        ListNode p1=head;
+        ListNode p2=head;
+        while(p2.next!=null&&p2.next.next!=null){
+            p1=p1.next;
+            p2=p2.next.next;
+        }
+
+        //Reverse the half after middle  1->2->3->4->5->6 to 1->2->3->6->5->4
+        ListNode preMiddle=p1;
+        ListNode preCurrent=p1.next;
+        while(preCurrent.next!=null){
+            ListNode current=preCurrent.next;
+            preCurrent.next=current.next;
+            current.next=preMiddle.next;
+            preMiddle.next=current;
+        }
+
+        //Start reorder one by one  1->2->3->6->5->4 to 1->6->2->5->3->4
+        p1=head;
+        p2=preMiddle.next;
+        while(p1!=preMiddle){
+            preMiddle.next=p2.next;
+            p2.next=p1.next;
+            p1.next=p2;
+            p1=p2.next;
+            p2=preMiddle.next;
+        }
+    }
+
+    public static void main(String[] args){
+        ListNode a = new ListNode(1);
+        a.next = new ListNode(2);
+        a.next.next = new ListNode(3);
+        a.next.next.next = new ListNode(4);
+        a.next.next.next.next = new ListNode(5);
+        Solution_143 sl = new Solution_143();
+        sl.reorderList(a);
+    }
+}
