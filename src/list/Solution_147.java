4982aaf7ad3e2afa24b14f196f79688864eb34e0
wxyBUPT <746451950@qq.com>
2016-09-19 12:06:08 +0800

    链表的插入排序

---
+++ src/list/Solution_147.java
@@ -0,0 +1,30 @@
+package list;
+
+/**
+ * Created by xiyuanbupt on 9/19/16.
+ */
+public class Solution_147 {
+
+    public ListNode insertionSortList(ListNode head) {
+        if(head==null||head.next==null)return head;
+        ListNode headPre = new ListNode(Integer.MIN_VALUE);
+        headPre.next = head;
+        ListNode curr = head.next;
+        ListNode tail = head;
+        tail.next = null;
+        while (curr!=null){
+            ListNode tmp = curr;
+            ListNode can = headPre;
+            curr = curr.next;
+
+            while (can.next != null && can.next.val < tmp.val){
+                can = can.next;
+            }
+
+            tmp.next = can.next;
+            can.next = tmp;
+
+        }
+        return headPre.next;
+    }
+}
