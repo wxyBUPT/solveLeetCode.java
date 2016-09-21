2597cf6bdf4fb34691721656439d19cc9b5da3f8
wxyBUPT <746451950@qq.com>
2016-09-21 20:04:33 +0800

    判断字符串的格式是否相同

---
+++ src/string/Solution_205.java
@@ -0,0 +1,40 @@
+package string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by xiyuanbupt on 9/21/16.
+ */
+public class Solution_205 {
+
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character,Character> map = new HashMap<>();
+        Map<Character,Character> map1 = new HashMap<>();
+        if(s.length() != t.length())return false;
+        int len = s.length();
+        Character c1;
+        Character c2;
+        for(int i = 0;i<len;i++){
+            c1 = s.charAt(i);
+            c2 = t.charAt(i);
+            System.out.println(c1 + " " + c2);
+            if(map.containsKey(c1) && !map1.containsKey(c2)){
+                return false;
+            }
+            else if( !map.containsKey(c1) && map1.containsKey(c2)){
+                return false;
+            }else if(!map.containsKey(c1) && !map1.containsKey(c2)){
+                map.put(c1,c2);
+                map1.put(c2,c1);
+            }else {
+                if(map.get(c1)!=c2 || map1.get(c2)!=c1)return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        Solution_205 sl = new Solution_205();
+        sl.isIsomorphic("ab","aa");
+    }
+}
