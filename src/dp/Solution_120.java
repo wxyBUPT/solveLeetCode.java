d700705bebfca797863b4d5e714631b72b9d57a1
wxyBUPT <746451950@qq.com>
2016-09-22 13:48:32 +0800

    使用动态规划计算最短路径

---
+++ src/dp/Solution_120.java
@@ -0,0 +1,36 @@
+package dp;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by xiyuanbupt on 9/22/16.
+ */
+public class Solution_120 {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle.size()==0)return 0;
+        List<List<Integer>> res = new ArrayList<>(triangle.size());
+        res.add(new ArrayList<>(triangle.get(0)));
+        for(int i = 1;i<triangle.size();i++){
+            List<Integer> hi = res.get(i-1);
+            List<Integer> cu = triangle.get(i);
+            List<Integer> lineRes = new ArrayList<>(cu.size());
+
+            int len = cu.size() -1;
+            /**
+             * 处理第一个元素
+             */
+            lineRes.add(cu.get(0) + hi.get(0));
+            for(int j = 1;j<len;j++){
+                lineRes.add(cu.get(j) + Math.min(
+                        hi.get(j-1),hi.get(j)
+                ));
+            }
+            lineRes.add(cu.get(len) + hi.get(len-1));
+            res.add(lineRes);
+        }
+
+        return Collections.min(res.get(triangle.size()-1));
+    }
+}
--- src/tree/Solution_337.java
+++ src/tree/Solution_337.java
@@ -5,7 +5,7 @@ package tree;
  */
 public class Solution_337 {
 
-    public int rob(TreeNode root) {
+    public int rob1(TreeNode root) {
         if(root==null)return 0;
         Node nodeRoot = buildTree(root);
         return Math.max(robRoot(nodeRoot),passRoot(nodeRoot));
@@ -64,5 +64,23 @@ public class Solution_337 {
         }else {
             return null;
         }
+    }
+
+    public int rob(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int val = 0;
+
+        if (root.left != null) {
+            val += rob(root.left.left) + rob(root.left.right);
+        }
+
+        if (root.right != null) {
+            val += rob(root.right.left) + rob(root.right.right);
+        }
+
+        return Math.max(val + root.val, rob(root.left) + rob(root.right));
     }
 }
