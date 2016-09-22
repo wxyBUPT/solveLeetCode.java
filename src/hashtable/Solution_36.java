8f521db43f5d9c16fc646c35113c1fb4e51a43b9
wxyBUPT <746451950@qq.com>
2016-09-22 10:44:35 +0800

    判断是否合法的数独

---
+++ src/hashtable/Solution_36.java
@@ -0,0 +1,26 @@
+package hashtable;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by xiyuanbupt on 9/22/16.
+ */
+public class Solution_36 {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>();
+        for(int i = 0;i<9;i++){
+            for(int j = 0;j<9;++j){
+                char num = board[i][j];
+                if(num!='.'){
+                    if(
+                            !seen.add(num + "is seen in row" + i)||
+                                    !seen.add(num + "is seen in column" + j)||
+                                    !seen.add(num+"is seen in block" + i/3 + " " + j/3)
+                            )return false;
+                }
+            }
+        }
+        return true;
+    }
+}
