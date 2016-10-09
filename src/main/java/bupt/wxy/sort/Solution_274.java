2fbb1dbf406054bd009842238fdff6ff3be0e347
wxyBUPT <746451950@qq.com>
2016-10-09 09:48:48 +0800

    作者文章引用

--- src/main/java/bupt/wxy/sort/Solution_274.java
+++ src/main/java/bupt/wxy/sort/Solution_274.java
@@ -1,6 +1,7 @@
 package bupt.wxy.sort;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Created by xiyuanbupt on 9/14/16.
@@ -10,7 +11,19 @@ public class Solution_274 {
     public int hIndex(int[] citations) {
         if(citations.length==0)return 0;
         Arrays.sort(citations);
-        int h = citations[citations.length -1];
+        int len = citations.length;
+        int h = len;
+        while (h > 0&& citations[len-h]<h ){
+            h--;
+        }
         return h;
+    }
+
+    public static void main(String[] args){
+        Solution_274 sl = new Solution_274();
+        int[] citations = {3, 0, 6, 1, 5};
+        int res = sl.hIndex(citations);
+        System.out.println(res);
+
     }
 }
