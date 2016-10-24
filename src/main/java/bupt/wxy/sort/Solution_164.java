f3fb23799274ad8c21f39e6e526b49d80dca0da9
wxyBUPT <746451950@qq.com>
2016-10-24 12:18:21 +0800

    使用桶排序

--- src/main/java/bupt/wxy/sort/Solution_164.java
+++ src/main/java/bupt/wxy/sort/Solution_164.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * Created by xiyuanbupt on 10/24/16.
@@ -19,10 +20,41 @@ import java.util.List;
 // 使用桶排序
 public class Solution_164 {
 
-    
 
-    // 下面的想法是自己规规矩矩的写桶排序
-    public int maximumGap(int[] nums) {
+    public int maximumGap(int[] nums){
+        if(nums==null||nums.length<2)return 0;
+
+        int max = nums[0];
+        int min = nums[0];
+        for(int i:nums){
+            min = Math.min(min,i);
+            max = Math.max(max,i);
+        }
+
+        int gap = (int)Math.ceil((double)(max-min)/(nums.length-1));
+        int[] bucketsMIN = new int[nums.length-1];
+        int[] bucketsMAX = new int[nums.length-1];
+        Arrays.fill(bucketsMIN,Integer.MAX_VALUE);
+        Arrays.fill(bucketsMAX,Integer.MIN_VALUE);
+        for(int i:nums){
+            if(i==min||i==max)continue;
+            int idx = (i-min)/gap;
+            bucketsMAX[idx] = Math.max(i,bucketsMAX[idx]);
+            bucketsMIN[idx] = Math.min(i,bucketsMIN[idx]);
+        }
+
+        int maxGap = Integer.MIN_VALUE;
+        int previous = min;
+        for(int i = 0;i<nums.length-1;i++){
+            if(bucketsMIN[i] == Integer.MAX_VALUE || bucketsMAX[i] == Integer.MIN_VALUE)continue;
+            maxGap = Math.max(maxGap,bucketsMIN[i] - previous);
+            previous = bucketsMAX[i];
+        }
+        return Math.max(maxGap,max-previous);
+    }
+
+    // 下面的想法是自己意淫的桶排序
+    public int mmaximumGap(int[] nums) {
         if(nums.length<2)return 0;
         List<Integer> res = new ArrayList<>();
         for(int i :nums){
