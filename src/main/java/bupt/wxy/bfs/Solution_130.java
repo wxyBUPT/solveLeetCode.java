3fd92c710743c38d0e009c7507121f4f5080037d
wxyBUPT <746451950@qq.com>
2016-11-09 11:55:16 +0800

    bfs

--- src/main/java/bupt/wxy/bfs/Solution_130.java
+++ src/main/java/bupt/wxy/bfs/Solution_130.java
@@ -3,25 +3,48 @@ package bupt.wxy.bfs;
 
 /**
  * Created by xiyuanbupt on 10/11/16.
+ Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
+
+ A region is captured by flipping all 'O's into 'X's in that surrounded region.
+
+ For example,
+ X X X X
+ X O O X
+ X X O X
+ X O X X
+ After running your function, the board should be:
+
+ X X X X
+ X X X X
+ X X X X
+ X O X X
  */
 public class Solution_130 {
 
     public void solve(char[][] board) {
-        if(board.length==0)return;
-        int h = board.length;
-        int w = board[0].length;
-        boolean[][] change = new boolean[h][w];
-
-        for(int i = 0;i<h;i++){
-            for(int j = 0;j<w;j++){
-                System.out.println(change[i][j]);
+        int row = board.length;
+        if(row==0)return;
+        int col = board[0].length;
+        for(int i = 0;i<row;i++)
+            for(int j = 0;j<col;j++){
+                if(board[i][j] == 'X') {
+                    boolean bot = i + 1 >= row || board[i+1][j] == 'O';
+                    boolean right = j+1 >=  col || board[i][j+1] == 'O';
+                    if(!bot||!right) {
+                        bfs(board, i, j);
+                    }
+                }
             }
-        }
     }
 
-    public static void main(String[] args){
-        Solution_130 sl = new Solution_130();
-        char[][] a = {{1,2},{2,3}};
-        sl.solve(a);
+    void bfs(char[][] board,int i,int j){
+        board[i][j] = 'O';
+        // top
+        if(i-1>=0 && board[i-1][j] == 'X')bfs(board,i-1,j);
+        if(i+1<board.length && board[i+1][j] == 'X')bfs(board,i+1,j);
+        if(j-1>=0 && board[i][j-1] == 'X')bfs(board,i,j-1);
+        if(j+1<board[0].length && board[i][j+1] == 'X')bfs(board,i,j+1);
     }
+
+
 }
